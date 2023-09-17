1eed3b9e727b78efa93be8a01d5833d9e69660de
Guillermo Suarez <gguillermosuarezz@gmail.com>
2023-09-17 16:07:45 -0300

    Avance inicialización aleatoria

---
+++ kmeans.py
@@ -0,0 +1,57 @@
+import copy
+import random as rnd
+
+from funciones import calcularDistancia, actualizarCentroides, separarPorClusters
+
+def marcarCentroidesAleatorios(k, puntos):
+    centroides = []
+    n = len(puntos)
+    for i in range(k):
+        j = rnd.randint(0, n - 1)
+        centroides.append([puntos[j][0], puntos[j][1]])
+        puntos[j][2] = i + 1
+    return centroides, puntos
+
+def kMeans(k, puntos, centroides):
+
+    iteraciones = []
+
+    puntosQueCambiaron = 1
+
+    while puntosQueCambiaron > 0:
+
+        puntosQueCambiaron = 0
+
+        # Para cada punto...
+        for punto in puntos:
+            nroClusterActual = punto[2]
+            distanciaActual = punto[3]
+
+            # ...calculamos las distancias con todos los otros centroides
+            distancias = []
+            for centroide in centroides:
+                distancia = calcularDistancia(centroide, punto)
+                distancias.append(distancia)
+
+            # Buscamos, entre todas esas distancias, la más chica que, además, sea menor a la actual
+            for j, distancia in enumerate(distancias):
+                if distancia < distanciaActual:
+                    distanciaActual = distancia
+                    nroClusterActual = j + 1
+            
+            # Si va a cambiar el Nº de cluster, se aumenta en 1 el contador de puntos que cambiaron de cluster
+            if nroClusterActual != punto[2]:
+                puntosQueCambiaron = puntosQueCambiaron + 1
+
+            # Se reemplazan los valores
+            punto[2] = nroClusterActual
+            punto[3] = distanciaActual
+        
+        iteracion = []
+        iteracion.append(copy.deepcopy(centroides))
+        iteracion.append(copy.deepcopy(puntos))
+        iteraciones.append(iteracion)
+        if puntosQueCambiaron > 0:
+            centroides = actualizarCentroides(k, puntos)
+
+    return list(iteraciones)
--- main.py
+++ main.py
@@ -1,39 +1,18 @@
-import csv
-import random
-import math as m
+from funciones import abrirCSV, separarPorClusters
+from kmeans import marcarCentroidesAleatorios, kMeans
+
 import matplotlib.pyplot as plt
 
-# Cantidad de clusters (1 < k < 6)
 k = 5
+puntos = abrirCSV('Datasets/dataset_1.csv')
+centroides, puntos = marcarCentroidesAleatorios(k, puntos)
+iteraciones = kMeans(k, puntos, centroides)
 
-with open("Datasets/dataset_1.csv", 'r') as dataset:
-    reader = csv.reader(dataset, delimiter=";")
-    encabezados = next(reader)
-    puntosCSV = list(reader)
-
-puntos = []
-puntosX = []
-puntosY = []
-
-# Pasar a float
-for punto in puntosCSV:
-    x, y = punto[0], punto[1]
-    x = float(x.replace(',', '.'))
-    y = float(y.replace(',', '.'))
-    puntos.append([x, y, 0])
-    puntosX.append(x)
-    puntosY.append(y)
-
-# Cada punto tiene [cordX, cordY, cluster al que pertence]
-# Cluster = 0 ----> todavía no se asignó un cluster
-
-# print('Este dataset tiene ' + str(len(puntos)) + ' puntos.')
-
-minX = puntos[0][0]
-maxX = puntos[0][0]
-minY = puntos[0][1]
-maxY = puntos[0][1]
-
+# Se buscan los máximos y mínimos por eje
+minX = 9999.0
+maxX = -9999.0
+minY = 9999.0
+maxY = -9999.0
 for punto in puntos:
     # Encontrar valor máximo y mínimo de eje X
     if punto[0] < minX:
@@ -47,48 +26,8 @@ for punto in puntos:
     elif punto[1] > maxY:
         maxY = punto[1]
 
-# print('El eje X va desde ' + str(minX) + ' hasta ' + str(maxX))
-# print('El eje Y va desde ' + str(minY) + ' hasta ' + str(maxY))
-
-# Crear los k centroides aleatoriamente
-centroides = []
-centroidesX = []
-centroidesY = []
-centroidesCords = []
-for i in range(k):
-    rndX = random.random()
-    rndY = random.random()
-    centX = minX + (maxX - minX) * rndX
-    centY = minY + (maxY - minY) * rndY
-    centroides.append([centX, centY])
-    centroidesX.append(centX)
-    centroidesY.append(centY)
-
-# for i in range(0, len(centroides)):
-#     print('Centroide ' + str(i + 1) + ': ' + str(centroides[i]))
-
-# Iteración 1 - asignación de clústers
-for punto in puntos:
-    distancias = []
-    for centroide in centroides:
-        distancias.append(m.sqrt((punto[0] - centroide[0])**2 + (punto[1] - centroide[1])**2))
-    distanciaMin = distancias[0]
-    for i in range(0, len(distancias)):
-        if distancias[i] <= distanciaMin:
-            punto[2] = i + 1
-            distanciaMin = distancias[i]
-
-# Creamos la lista de clusters
-clustersCords = []
-for i in range(k):
-    clusterX = []
-    clusterY = []
-    for punto in puntos:
-        if(punto[2] == i + 1):
-            clusterX.append(punto[0])
-            clusterY.append(punto[1])
-    clustersCords.append([clusterX, clusterY])
-
+# Definir límites de los ejes para los gráficos
+# Se le suma 10% (0.1) a ambos lados a cada eje
 if(minX < 0):
     limMinX = minX + (minX * 0.1)
 else:
@@ -105,8 +44,9 @@ else:
 if(maxY < 0):
     limMaxY = maxY - (maxY * 0.1)
 else:
-    limMaxY = maxY + (maxY * 0.1)
+    limMaxY = maxY + (maxY * 0.1)    
 
+# Como son 5 clústers como máximo, 5 colores
 colores = [
     [1, 0, 0],
     [0, 1, 0],
@@ -115,34 +55,52 @@ colores = [
     [0, 1, 1]
 ]
 
-# En un gráfico mostramos los puntos y los centroides resultados de la Iteración 1
-plt.subplot(1, 2, 1)
-plt.gca().set_facecolor((0, 0, 0, 0.75))
-plt.scatter(puntosX, puntosY, label='Punto sin agrupar', color=(1, 1, 1), s=20.0, edgecolors='black')
-for i in range (len(centroides)):
-    plt.scatter(centroides[i][0], centroides[i][1], label='Centroide ' + str(i + 1), color=colores[i], s=100.0, edgecolors='black')
-plt.xlim(limMinX, limMaxX)
-plt.ylim(limMinY, limMaxY)
+plt.figure()
+
+x = [punto[0] for punto in puntos]
+y = [punto[1] for punto in puntos]
+plt.scatter(x, y, label = 'Punto sin asignar' , color = [1, 1, 1], s = 20.0, edgecolors = 'black')
+
+for h, centroide in enumerate(centroides):
+        x = centroide[0]
+        y = centroide[1]
+        plt.scatter(x, y, label = 'Centroide ' + str(h + 1), color = colores[h], s = 100.0, edgecolors = 'black')
+
 plt.xlabel('Eje X')
 plt.ylabel('Eje Y')
-plt.title('Antes de agrupar')
-plt.legend()
-
-# En otro gráfico mostramos como quedarían los clusters después de la Iteración 1
-plt.subplot(1, 2, 2)
-plt.gca().set_facecolor((0, 0, 0, 0.75))
-for i in range(len(clustersCords)):
-    # print('Clúster ' + str(i + 1) + ' tiene ' + str(len(clustersCords[i][0])) + ' valores de eje X y ' + str(len(clustersCords[i][1])) + ' valores de eje Y.')
-    plt.scatter(clustersCords[i][0], clustersCords[i][1], label='Punto del clúster ' + str(i + 1), color=(colores[i]), s=20.0, edgecolors='black')
-for i in range (len(centroides)):
-    plt.scatter(centroides[i][0], centroides[i][1], label='Centroide ' + str(i + 1), color=colores[i], s=100.0, edgecolors='black')
 plt.xlim(limMinX, limMaxX)
 plt.ylim(limMinY, limMaxY)
-plt.xlabel('Eje X')
-plt.ylabel('Eje Y')
-plt.title('Después de agrupar')
-plt.legend()
+plt.title('Estado inicial')
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
-plt.suptitle('Iteración 1 - NO SE VUELVEN A AJUSTAR LOS CENTROIDES')
 plt.tight_layout()
+plt.show(block = False)
+
+for i, iteracion in enumerate(iteraciones):
+    plt.figure()
+
+    centroides = iteracion[0]
+    puntos = iteracion[1]
+    clusters = separarPorClusters(k, puntos)
+
+    for j, cluster in enumerate(clusters):
+        x = [punto[0] for punto in cluster]
+        y = [punto[1] for punto in cluster]
+        plt.scatter(x, y, label = 'Punto del clúster ' + str(j + 1), color = colores[j], s = 20.0, edgecolors = 'black')
+
+    for h, centroide in enumerate(centroides):
+        x = centroide[0]
+        y = centroide[1]
+        plt.scatter(x, y, label = 'Centroide ' + str(h + 1), color = colores[h], s = 100.0, edgecolors = 'black')
+
+    plt.xlabel('Eje X')
+    plt.ylabel('Eje Y')
+    plt.xlim(limMinX, limMaxX)
+    plt.ylim(limMinY, limMaxY)
+    plt.title('Iteración ' + str(i + 1))
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+
+    plt.tight_layout()
+    plt.show(block = False)
+
 plt.show()
