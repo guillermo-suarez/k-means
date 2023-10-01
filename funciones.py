000c4078da94b97c8d90550794b53a093251ce8f
srebata <lmehowicz@gmail.com>
2023-10-01 20:43:05 -0300

    Modularización de ch_score y mejora de nombres

--- funciones.py
+++ funciones.py
@@ -38,3 +38,51 @@ def actualizarCentroide(cluster, centroide):
         y = centroide[1]
     nuevoCentroide = [x, y]
     return nuevoCentroide
+
+#-------------A partir de acá son funciones específicas para Calinski-Harabasz-------------
+
+def norma_sin_raiz(puntoA, puntoB):
+    #Función que simula el calculo de la norma entre dos puntos. No utiliza la raíz cuadrada
+    #debido a que en CH se eleva al cuadrado la norma, haciendo así que se simplique la raíz.
+    return ((puntoA[0]-puntoB[0])**2 + (puntoA[1]-puntoB[1])**2)
+
+def calculo_bgss(dataset, centroides, clusters):
+    #Función que devuelve la dispersión inter-cluster.
+    #El baricentro es el centroide del dataset.
+    sumatoria = 0
+    cant_puntos = 0
+    distancia_baricentro = 0
+    baricentro = actualizarCentroide(dataset, 0)
+    for i, cluster in enumerate(clusters):
+        cant_puntos = len(cluster)
+        distancia_baricentro = norma_sin_raiz(centroides[i], baricentro)
+        sumatoria += (cant_puntos * distancia_baricentro)
+    return sumatoria
+
+def calculo_wgss(centroides, clusters):
+    #Función que devuelve la dispersión intra-cluster.
+    sumatoria_total = 0
+    for i, cluster in enumerate(clusters):
+        sumatoria = 0
+        for punto in cluster:
+            sumatoria += norma_sin_raiz(centroides[i], punto)
+        sumatoria_total += sumatoria
+    return sumatoria_total
+
+def ch_score(puntos, iteraciones):
+    #Función que permite calcular el indice Calinski-Harabasz.
+    #Se utiliza solamente la iteración final, por eso el [-1].
+    #UI: siglas para última iteración.
+    cant_puntos = len(puntos)
+    bgss = 0
+    wgss = 0
+    score = 0
+    centroides_UI = iteraciones[-1][0]
+    clusters_UI = iteraciones[-1][1]
+    k = len(centroides_UI)
+    bgss = calculo_bgss(puntos, centroides_UI, clusters_UI)
+    wgss = calculo_wgss(centroides_UI, clusters_UI)
+    score = ((bgss/wgss) * ((cant_puntos - k)/(k-1)))
+    return score
+
+#-------------Hasta acá son funciones específicas para Calinski-Harabasz-------------
--- prueba.py
+++ prueba.py
@@ -1,60 +1,18 @@
-import kmeans as k_means
-import funciones as funciones_tp
+from funciones import abrirCSV, ch_score
+from kmeans import marcarCentroidesAleatorios, kMeans
 
-def ch_score():
-    #Permite calcular el indice Calinski-Harabasz
-    cant_pruebas = 2
-    puntos = funciones_tp.abrirCSV('Datasets/dataset_1.csv')
-    cant_puntos = len(puntos)
-    #print("Cantidad de puntos: " + str(cant_puntos))
-    #se prueba en todo el rango de los k posibles
-    for k in range(2,6):
-        ch_score_promedio = 0
-        #print("Para k = " + str(k))
-        for i in range(cant_pruebas):
-            #print("Prueba " + str(i))
-            bgss_prueba_i = 0
-            wgss_prueba_i = 0
-            ch_score_prueba_i = 0
-            centroides, puntos = k_means.marcarCentroidesAleatorios(k, puntos)
-            iteraciones = k_means.kMeans(k, puntos, centroides)
-            #Se utiliza solamente la iteración final, por eso el [-1]
-            #UI: siglas para última iteración
-            centroides_UI = iteraciones[-1][0]
-            clusters_UI = iteraciones[-1][1]
-            bgss_prueba_i = bgss(puntos, centroides_UI, clusters_UI)
-            wgss_prueba_i = wgss(centroides_UI, clusters_UI)
-            ch_score_prueba_i = ((bgss_prueba_i/wgss_prueba_i) * ((cant_puntos - k)/(k-1)))
-            ch_score_promedio +=  ch_score_prueba_i
-            # print("BGSS = " + str(bgss_prueba_i))
-            # print("WGSS = " + str(wgss_prueba_i))
-            # print("Diferencia: " + str(bgss_prueba_i - wgss_prueba_i))
-            # print("CH de la prueba " + str(ch_score_prueba_i))
-        ch_score_promedio /= cant_pruebas 
-        print("Para k = " + str(k) + " se obtuvo un CH promedio de " + str(ch_score_promedio))
-        #print("----------------------------")
-
-def bgss(dataset, centroides, clusters):
-    #Función que devuelve la dispersión inter-cluster
-    #El baricentro se refiere al centroide del dataset
-    sumatoria = 0
-    cant_puntos = 0
-    distancia_baricentro = 0
-    baricentro = funciones_tp.actualizarCentroide(dataset, 0)
-    for i, cluster in enumerate(clusters):
-        cant_puntos = len(cluster)
-        distancia_baricentro = funciones_tp.calcularDistancia(centroides[i], baricentro)
-        sumatoria += (cant_puntos * distancia_baricentro)
-    return sumatoria
-
-def wgss(centroides, clusters):
-    #Función que devuelve la dispersión intra-cluster
-    sumatoria_total = 0
-    for i, cluster in enumerate(clusters):
-        sumatoria = 0
-        for punto in cluster:
-            sumatoria += funciones_tp.calcularDistancia(centroides[i], punto)
-        sumatoria_total += sumatoria
-    return sumatoria_total
-
-ch_score()
+cant_pruebas = 10
+puntos = abrirCSV('Datasets/dataset_1.csv')
+#se prueba en todo el rango de los k posibles
+for k in range(2,6):
+    ch_score_promedio = 0
+    for i in range(cant_pruebas):
+        bgss_prueba_i = 0
+        wgss_prueba_i = 0
+        ch_score_prueba_i = 0
+        centroides, puntos = marcarCentroidesAleatorios(k, puntos)
+        iteraciones = kMeans(k, puntos, centroides)
+        ch_score_prueba_i = ch_score(puntos, iteraciones)
+        ch_score_promedio +=  ch_score_prueba_i
+    ch_score_promedio /= cant_pruebas 
+    print("Para k = " + str(k) + " se obtuvo un CH promedio de " + str(ch_score_promedio))
