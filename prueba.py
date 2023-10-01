4848378d32a129a1cf57ad7f641e523a31f92274
srebata <lmehowicz@gmail.com>
2023-10-01 11:53:02 -0300

    Finaliza primera versión

--- prueba.py
+++ prueba.py
@@ -3,25 +3,48 @@ import funciones as funciones_tp
 
 def ch_score():
     #Permite calcular el indice Calinski-Harabasz
-    cant_pruebas = 0
-    puntos = funciones_tp.abrirCSV('Datasets/dataset_2.csv')
-    total_observaciones = len(puntos)
+    cant_pruebas = 2
+    puntos = funciones_tp.abrirCSV('Datasets/dataset_1.csv')
+    cant_puntos = len(puntos)
+    #print("Cantidad de puntos: " + str(cant_puntos))
     #se prueba en todo el rango de los k posibles
-    for k in range(1,6):
+    for k in range(2,6):
         ch_score_promedio = 0
+        #print("Para k = " + str(k))
         for i in range(cant_pruebas):
+            #print("Prueba " + str(i))
+            bgss_prueba_i = 0
+            wgss_prueba_i = 0
+            ch_score_prueba_i = 0
             centroides, puntos = k_means.marcarCentroidesAleatorios(k, puntos)
             iteraciones = k_means.kMeans(k, puntos, centroides)
-            #Se utiliza solamente la iteración final
-            centroides_ultima_iteracion = iteraciones[-1][0]
-            clusters = iteraciones[-1][1]
+            #Se utiliza solamente la iteración final, por eso el [-1]
+            #UI: siglas para última iteración
+            centroides_UI = iteraciones[-1][0]
+            clusters_UI = iteraciones[-1][1]
+            bgss_prueba_i = bgss(puntos, centroides_UI, clusters_UI)
+            wgss_prueba_i = wgss(centroides_UI, clusters_UI)
+            ch_score_prueba_i = ((bgss_prueba_i/wgss_prueba_i) * ((cant_puntos - k)/(k-1)))
+            ch_score_promedio +=  ch_score_prueba_i
+            # print("BGSS = " + str(bgss_prueba_i))
+            # print("WGSS = " + str(wgss_prueba_i))
+            # print("Diferencia: " + str(bgss_prueba_i - wgss_prueba_i))
+            # print("CH de la prueba " + str(ch_score_prueba_i))
+        ch_score_promedio /= cant_pruebas 
+        print("Para k = " + str(k) + " se obtuvo un CH promedio de " + str(ch_score_promedio))
+        #print("----------------------------")
 
 def bgss(dataset, centroides, clusters):
     #Función que devuelve la dispersión inter-cluster
-    centroide_dataset = funciones_tp.actualizarCentroide(dataset,0)
+    #El baricentro se refiere al centroide del dataset
     sumatoria = 0
+    cant_puntos = 0
+    distancia_baricentro = 0
+    baricentro = funciones_tp.actualizarCentroide(dataset, 0)
     for i, cluster in enumerate(clusters):
-        sumatoria += (len(cluster) * funciones_tp.calcularDistancia(centroides[i],centroide_dataset))
+        cant_puntos = len(cluster)
+        distancia_baricentro = funciones_tp.calcularDistancia(centroides[i], baricentro)
+        sumatoria += (cant_puntos * distancia_baricentro)
     return sumatoria
 
 def wgss(centroides, clusters):
@@ -33,3 +56,5 @@ def wgss(centroides, clusters):
             sumatoria += funciones_tp.calcularDistancia(centroides[i], punto)
         sumatoria_total += sumatoria
     return sumatoria_total
+
+ch_score()
