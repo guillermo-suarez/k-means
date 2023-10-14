ee38f9174f901fb7a820f427e85e4648b5fdad0f
solmala <92184025+solmala@users.noreply.github.com>
2023-10-14 17:17:19 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,4 +1,5 @@
 import csv
+import math as m
 
 def abrirCSV(path: str):
     with open(path, 'r') as dataset:
@@ -6,65 +7,66 @@ def abrirCSV(path: str):
         encabezados = next(reader)
         datos = list(reader)
     puntos = []
+    etiquetas = []
     for dato in datos:
         x = float(dato[0].replace(',', '.'))
         y = float(dato[1].replace(',', '.'))
-        cluster = int(0)
-        punto = [x, y, cluster]
+        punto = [x, y]
         puntos.append(punto)
-    return puntos
 
-def separarPorClusters(k, puntos):
+        etiqueta = int(0)
+        etiquetas.append(etiqueta)
+    return puntos, etiquetas
+
+def separarPorClusters(k, puntos, etiquetas):
     clusters = []
     for i in range(k):
         cluster = []
         clusters.append(cluster)
-    for punto in puntos:
-        clusters[punto[2] - 1].append([punto[0], punto[1]]) 
+    for i, punto in enumerate(puntos):
+        clusters[etiquetas[i] - 1].append(punto) 
     return clusters
 
 def calcularDistancia(puntoA, puntoB):
-    return m.sqrt((puntoA[0]-puntoB[0])**2 + (puntoA[1]-puntoB[1])**2)
+    return ((puntoA[0]-puntoB[0])**2 + (puntoA[1]-puntoB[1])**2)
 
-def actualizarCentroide(cluster, centroide):
-    if len(cluster) > 0:
-        valoresX = [punto[0] for punto in cluster]
-        valoresY = [punto[1] for punto in cluster]
-        x = min(valoresX) + ((max(valoresX) - min(valoresX)) / 2.0)
-        y = min(valoresY) + ((max(valoresY) - min(valoresY)) / 2.0)
-    else:
-        x = centroide[0]
-        y = centroide[1]
-    nuevoCentroide = [x, y]
+def getPuntoMedio(grupoDePuntos):
+    nuevoCentroide = None
+    if len(grupoDePuntos) > 0:
+        valoresX = [punto[0] for punto in grupoDePuntos]
+        valoresY = [punto[1] for punto in grupoDePuntos]
+        x = sum(valoresX) / len(valoresX)
+        y = sum(valoresY) / len(valoresY)
+        nuevoCentroide = [x, y]
     return nuevoCentroide
 
 def calcularBGSS(dataset, centroides, clusters):
     # Función que devuelve la dispersión inter-cluster.
     # El baricentro es el centroide del dataset.
-    bgss = 0
+    BGSS = 0
     baricentro = getPuntoMedio(dataset)
     for i, cluster in enumerate(clusters):
         nCluster = len(cluster)
         distanciaBaricentro = calcularDistancia(centroides[i], baricentro)
-        bgss += (nCluster * distanciaBaricentro)
-    return bgss
+        BGSS += (nCluster * distanciaBaricentro)
+    return BGSS
 
 def calcularWGSS(centroides, clusters):
     # Función que devuelve la dispersión intra-cluster.
-    wgss = 0
+    WGSS = 0
     for i, cluster in enumerate(clusters):
         sumatoria = 0
         for punto in cluster:
             sumatoria += calcularDistancia(centroides[i], punto)
-        wgss += sumatoria
-    return wgss
+        WGSS += sumatoria
+    return WGSS
 
 def getCHScore(centroides, puntos, etiquetas):
     # Función que permite calcular el indice Calinski-Harabasz.
-    n = len(puntos)
+    N = len(puntos)
     k = len(centroides)
     clusters = separarPorClusters(k, puntos, etiquetas)
-    bgss = calcularBGSS(puntos, centroides, clusters)
-    wgss = calcularWGSS(centroides, clusters)
-    chScore = ((bgss / wgss) * ((n - k) / (k - 1)))
+    BGSS = calcularBGSS(puntos, centroides, clusters)
+    WGSS = calcularWGSS(centroides, clusters)
+    chScore = ((BGSS / WGSS) * ((N - k) / (k - 1)))
     return chScore
